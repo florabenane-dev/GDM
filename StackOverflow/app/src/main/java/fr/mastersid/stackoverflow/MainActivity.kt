566f3925618e15e5daab164e4ec49235a9b975f1
Flora BENANE <flora.benane.pro@gmail.com>
2026-01-19 12:21:59 +0100

    finished tp1

--- StackOverflow/app/src/main/java/fr/mastersid/stackoverflow/MainActivity.kt
+++ StackOverflow/app/src/main/java/fr/mastersid/stackoverflow/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -18,9 +21,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.mastersid.stackoverflow.data.Question
 import fr.mastersid.stackoverflow.ui.theme.StackOverflowTheme
 
@@ -43,7 +49,7 @@ fun QuestionsScreen(modifier: Modifier) {
     var onlyNotAnsweredQuestions: Boolean by rememberSaveable { mutableStateOf(false) }
 
     // la liste donnee en dur
-    val questionsAnswered = listOf(
+    val questionsListAnswered = listOf(
         Question(0, "Find node that is connected to a node", 1),
         Question(1, "pdfjsLib not initializing and is undefined", 2),
         Question(2, "Error while building bundle in maven", 2),
@@ -61,28 +67,27 @@ fun QuestionsScreen(modifier: Modifier) {
 
     // un etat pour la liste de question
     val questionsList = if (onlyNotAnsweredQuestions) {
-        questionsAnswered.sortedBy { question -> question.answerCount == 0 }
+        questionsListAnswered.sortedBy { question -> question.answerCount == 0 }
     } else {
-        questionsAnswered
+        questionsListAnswered
     }
 
     Scaffold(
         modifier = modifier,
         bottomBar = {
-            Row() {
-                Switch(
-                    checked = onlyNotAnsweredQuestions
-                    onCheckedChange = { checked -> onlyNotAnsweredQuestions = checked }
-                )
-                Text(
-                    stringResource(id = R.string.only_not_answered_questions)
-                )
+            SortByNotAnsweredSwitch(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                onlyNotAnsweredQuestions = onlyNotAnsweredQuestions
+            ) {
+                checked ->
+                onlyNotAnsweredQuestions = checked
             }
         }
     ) { innerPadding ->
         LazyColumn(
-
-
+            modifier = Modifier.padding(innerPadding),
+            contentPadding = PaddingValues(16.dp), //separation autour du texte
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(questionsList) { question -> QuestionsRow(question)}
         }
@@ -90,17 +95,58 @@ fun QuestionsScreen(modifier: Modifier) {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun QuestionsRow(question: Question) {
+    Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) { //espace entre les elts
+        Text(
+            text = question.title,
+            maxLines = 1, //pas de retour à la ligne
+            overflow = TextOverflow.Ellipsis, //pointillés finaux si text trop long
+            modifier = Modifier.weight(1f) //prend le max de place
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StackOverflowTheme {
-        Greeting("Android")
+        Text(
+            text = stringResource(id = R.string.answer_count, question.answerCount)
+        )
     }
 }
+
+@Composable
+fun SortByNotAnsweredSwitch(modifier: Modifier, onlyNotAnsweredQuestions: Boolean, onChange: (Boolean) -> Unit) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+    ) {
+        Switch(
+            checked =  onlyNotAnsweredQuestions,
+            onCheckedChange = onChange
+        )
+        Text(stringResource(id = R.string.only_not_answered_questions))
+    }
+}
+
+//prévisualisation
+@Preview(widthDp = 400)
+@Composable
+fun SortByNotAnsweredSwitchPreview() {
+    StackOverflowTheme {
+        SortByNotAnsweredSwitch(modifier = Modifier.padding(horizontal = 16.dp), onlyNotAnsweredQuestions = true) {
+        }
+    }
+}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    StackOverflowTheme {
+//        Greeting("Android")
+//    }
+//}
