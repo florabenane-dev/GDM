002dee5b0c93f8a32015838b0961844b733146c2
Flora Benane <flora.benane.pro@gmail.com>
2026-02-19 15:55:29 +0100

    feat: finish viewmodel with injection

--- StackOverflow/app/src/main/java/fr/mastersid/stackoverflow/viewmodel/QuestionsViewModel.kt
+++ StackOverflow/app/src/main/java/fr/mastersid/stackoverflow/viewmodel/QuestionsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import fr.mastersid.stackoverflow.Repository.SlackOverflowRepository
 import fr.mastersid.stackoverflow.data.Question
+import fr.mastersid.stackoverflow.data.QuestionsResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,9 +22,24 @@ class QuestionsViewModel @Inject constructor(
     private val _isUpdating = MutableLiveData(false)
     val isUpdating: LiveData<Boolean> = _isUpdating
 
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            //collect est bloquante et agit cm une boucle infinie en attente d'une new reponse
+            stackOverflowRepository.questionsResponse.collect { response ->
+                when (response) {
+                    is QuestionsResponse.Pending -> _isUpdating.postValue(true)
+                    is QuestionsResponse.Succes -> {
+                        _questions.postValue(response.list)
+                        _isUpdating.postValue(false)
+                    }
+                }
+            }
+        }
+    }
+
     fun updateQuestions() {
         viewModelScope.launch(Dispatchers.IO) {
-            stackOverflowRepository.
+            stackOverflowRepository.updateQuestionsInfo()
         }
 
     }
